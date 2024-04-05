dd7f3cecfefba5fc9aa8ae3a80005855ea2022e1
08namrata <acharyanamrata08@gmail.com>
2024-04-05 15:59:08 +0530

    healthcheck module added

--- src/app.js
+++ src/app.js
@@ -26,7 +26,7 @@ import commentRouter from "./routes/comment.routes.js"
 import likeRouter from "./routes/like.routes.js"
 import playlistRouter from "./routes/playlist.routes.js"
 import dashboardRouter from "./routes/dashboard.routes.js"
-
+import healthcheckRouter from "./routes/healthcheck.routes.js"
 //routes declaration
 app.use("/api/v1/users",router)
 app.use("/api/v1/videos",videoRouter)
@@ -36,5 +36,6 @@ app.use("/api/v1/comments", commentRouter)
 app.use("/api/v1/likes", likeRouter)
 app.use("/api/v1/playlists", playlistRouter)
 app.use("/api/v1/dashboard", dashboardRouter)
+app.use("/api/v1/healthcheck", healthcheckRouter)
 
 export {app}
